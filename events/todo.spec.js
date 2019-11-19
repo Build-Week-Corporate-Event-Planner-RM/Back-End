966043d47205314f6698add06a2ac066c3b2b06b
Muamer Kukic <kukicako@gmail.com>
2019-11-19 14:39:15 -0800

    added todo test

---
+++ events/todo.spec.js
@@ -0,0 +1,28 @@
+const db = require("../data/dbConfig");
+
+const { addTodo} = require("./events-model");
+
+describe("events model", function(){
+    describe('insert()', () => {
+        beforeEach(async () => {
+            await db("todos").truncate();
+          });
+
+        it("should insert todo", async function(){
+            const todo = await db("todos");
+            expect(todo).toHaveLength(0);
+            console.log(todo)
+            await addTodo({
+                event_id: 1,
+                name: "test vendor",
+                completed: 1,
+            })
+            const inserted = await db("todos")
+            expect(inserted).toHaveLength(1);
+        })
+    })
+
+})
+
+
+
