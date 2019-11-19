416d5e36354af569006f05cd28bf08cc3fa208cb
Emily_Richard <richard.emily.e@gmail.com>
2019-11-19 09:25:10 -0500

    added id to find user by id return object

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,71 +1,74 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const Users = require('./auth-model')
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("./auth-model");
 
-const secret = require('../config/secrets')
+const secret = require("../config/secrets");
 
-
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   const user = req.body;
 
-    const hash = bcrypt.hashSync(user.password, 12)
-    user.password = hash;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
 
-    Users.add(user)
-        .then(userN => {
-            const token = generateToken(userN)
-            res.status(200).json({token: token})
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(500).json(error);
-          });
+  Users.add(user)
+    .then(userN => {
+      const token = generateToken(userN);
+      res.status(200).json({ token: token });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json(error);
+    });
 });
 
-
-
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
-    Users.findBy({username})
-        .then(user => {
-            user = user[0]
-            if(user && bcrypt.compareSync(password, user.password)){
-                const token = generateToken(user);
-                res.status(200).json({
-                    message: `Welcome ${user.username}! Here's a token...`,
-                    token,
-                  });
-            }
-            else{
-                res.status(401).json({ message: 'Invalid Credentials' })
-            }
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(500).json({message: `error logging user in!`})
-        })
+  Users.findBy({ username })
+    .then(user => {
+      user = user[0];
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}! Here's a token...`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: `error logging user in!` });
+    });
 });
 
-router.get('/:id', (req, res) => {
-    let id = req.params.id;
+router.get("/:id", (req, res) => {
+  let id = req.params.id;
 
-    Users.findById(id)
+  Users.findById(id)
     .then(user => {
-        const username = user.username;
-        res.status(200).json({ username: username });
+      const username = user.username;
+      res.status(200).json({ id: id, username: username });
     })
-    .catch(err => res.status(500).json({ message: 'User with specified ID does not exist.', error: error }));
-})
+    .catch(err =>
+      res
+        .status(500)
+        .json({
+          message: "User with specified ID does not exist.",
+          error: error
+        })
+    );
+});
 
-function generateToken(user){
+function generateToken(user) {
   const payload = {
-      username: user.username,
-      password: user.password
+    username: user.username,
+    password: user.password
   };
-  const options ={
-      expiresIn: '1d'
+  const options = {
+    expiresIn: "1d"
   };
   return jwt.sign(payload, secret.jwtSecret, options);
 }
